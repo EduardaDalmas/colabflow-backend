7760bfb02d6b2fc88b039de7e4810eb9885629b7
Eduarda <eduarda.dalmas@dcgroupsaude.com.br>
2024-08-14 11:26:43 -0300

    rodando websocket

--- socket/chatSocket.js
+++ socket/chatSocket.js
@@ -5,27 +5,31 @@ const socketIo = require('socket.io');
 module.exports = function (app, socketPort) {
     const server = http.createServer(app);
     const io = socketIo(server, {
-        cors: {
-            origin: 'http://localhost:5173', // Substitua pela origem correta
-        },
+      cors: {
+        origin: 'http://localhost:5174',
+        methods: ['GET', 'POST'],
+        allowedHeaders: ['Content-Type'],
+        credentials: true,
+      },
     });
 
     io.on('connection', (socket) => {
         console.log('Usuário conectado', socket.id);
 
         socket.on('disconnect', (reason) => {
-            console.log('Usuário desconectado', socket.id);
+          console.log('Usuário desconectado', socket.id);
         });
 
         socket.on('set_username', (username) => {
-            socket.data.username = username;
+          socket.data.username = username;
         });
 
         socket.on('message', (text) => {
+            console.log('aqui', text);
             io.emit('received_message', {
-                text,
-                authorId: socket.id,
-                author: socket.data.username,
+              text,
+              authorId: socket.id,
+              author: socket.data.username,
             });
         });
     });
