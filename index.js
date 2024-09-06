ef6f95d6d750ca991d55bbbff45315d560d3a202
Guilhermegfssilva <guilhermefsilva2019@hotmail.com>
2024-09-05 23:28:00 -0300

    ajustes

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
 require('dotenv').config();
 const express = require('express');
-const app = express();
+const https = require('https');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const routes = require('./routes/index');
 const chatSocket = require('./socket/chatSocket');
 
+const app = express();
 const port = process.env.PORT || 3000;
 const socketPort = process.env.SOCKET_PORT || 3001;
 
-// Middleware para parsing de JSON
+// Caminho para os arquivos de certificado e chave
+const keyPath = '/etc/letsencrypt/live/colabflow.eastus.cloudapp.azure.com/privkey.pem';
+const certPath = '/etc/letsencrypt/live/colabflow.eastus.cloudapp.azure.com/fullchain.pem';
+
+// Ler os arquivos de certificado e chave
+const privateKey = fs.readFileSync(keyPath, 'utf8');
+const certificate = fs.readFileSync(certPath, 'utf8');
+
+// Configuração das credenciais
+const credentials = { key: privateKey, cert: certificate };
+
+const allowedOrigins = ['http://localhost:3000', 'https://colabflow.eastus.cloudapp.azure.com', 'https://black-desert-0361fb10f.5.azurestaticapps.net'];
+
+// Middleware para parsing de JSON e CORS
 app.use(express.json());
+app.use(cors({
+  origin: allowedOrigins,
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 // Usar as rotas
 app.use('/api', routes);
@@ -16,7 +38,7 @@ app.use('/api', routes);
 // Inicializar o WebSocket
 chatSocket(app, socketPort);
 
-// Iniciar o servidor HTTP (separado do WebSocket)
-app.listen(port, () => {
-    console.log(`HTTP server is running on port ${port}`);
+// Criar e iniciar o servidor HTTPS
+https.createServer(credentials, app).listen(port, () => {
+  console.log(`Servidor HTTPS rodando na porta ${port}`);
 });
--- socket/chatSocket.js
+++ socket/chatSocket.js
@@ -1,16 +1,29 @@
 const express = require('express');
-const http = require('http');
+const https = require('https');
+const fs = require('fs');
 const socketIo = require('socket.io');
 
+// Caminho para os arquivos de certificado e chave
+const keyPath = '/etc/letsencrypt/live/colabflow.eastus.cloudapp.azure.com/privkey.pem';
+const certPath = '/etc/letsencrypt/live/colabflow.eastus.cloudapp.azure.com/fullchain.pem';
+
+// Ler os arquivos de certificado e chave
+const privateKey = fs.readFileSync(keyPath, 'utf8');
+const certificate = fs.readFileSync(certPath, 'utf8');
+
+// Configuração das credenciais
+const credentials = { key: privateKey, cert: certificate };
+
 module.exports = function (app, socketPort) {
-    const server = http.createServer(app);
+    // Criar o servidor HTTPS
+    const server = https.createServer(credentials, app);
     const io = socketIo(server, {
-      cors: {
-        origin: 'http://localhost:5173',
-        methods: ['GET', 'POST'],
-        allowedHeaders: ['Content-Type'],
-        credentials: true,
-      },
+        cors: {
+            origin: 'https://black-desert-0361fb10f.5.azurestaticapps.net',
+            methods: ['GET', 'POST'],
+            allowedHeaders: ['Content-Type'],
+            credentials: true,
+        },
     });
 
     // Objeto para armazenar mensagens por sala
@@ -20,22 +33,22 @@ module.exports = function (app, socketPort) {
         console.log('Usuário conectado', socket.id);
 
         socket.on('disconnect', (reason) => {
-          console.log('Usuário desconectado', socket.id);
+            console.log('Usuário desconectado', socket.id);
         });
 
         socket.on('set_username', (username) => {
-          socket.data.username = username;
+            socket.data.username = username;
         });
 
         socket.on('join_room', (room) => {
             socket.join(room);
             socket.data.room = room;
-            //desconecta da sala antiga
+            // Desconecta da sala antiga
             if (socket.data.oldRoom) {
                 socket.leave(socket.data.oldRoom);
             }
             console.log(`${socket.data.username} entrou na sala ${room}`);
-            
+
             // Enviar o histórico de mensagens da sala para o usuário que acabou de entrar
             if (messagesByRoom[room]) {
                 socket.emit('room_history', messagesByRoom[room]);
@@ -44,7 +57,7 @@ module.exports = function (app, socketPort) {
             }
         });
 
-        //leave room
+        // Leave room
         socket.on('leave_room', (room) => {
             socket.leave(room);
             socket.data.oldRoom = room;
@@ -59,23 +72,19 @@ module.exports = function (app, socketPort) {
             console.log(authRoom);
 
             if (authRoom == room) {
-            const message = {
-                authorId: messageData.authorId,
-                author: messageData.author,
-                text: messageData.text,
-                data: messageData.data,
-                room: room,
-            };
-                      
-                
+                const message = {
+                    authorId: messageData.authorId,
+                    author: messageData.author,
+                    text: messageData.text,
+                    data: messageData.data,
+                    room: room,
+                };
+
                 // Armazenar a mensagem no histórico da sala
                 messagesByRoom[room].push(message);
 
                 // Emitir a mensagem para todos os usuários na sala
-
-
-                //emite mensagem para apenas a sala atual
-                if(room == authRoom){
+                if (room == authRoom) {
                     io.to(room).emit('received_message', message);
                 }
             } else {
