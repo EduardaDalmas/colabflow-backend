2bbce01a9cfd642cc7f43a846658c4812e1da92e
Guilhermegfssilva <guilhermefsilva2019@hotmail.com>
2024-09-14 15:01:35 -0300

    ajustes gerais autenticacao

--- index.js
+++ index.js
@@ -3,12 +3,25 @@ const express = require('express');
 const app = express();
 const routes = require('./routes/index');
 const chatSocket = require('./socket/chatSocket');
+const db = require('./config/db');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+
 
 const port = process.env.PORT || 3000;
 const socketPort = process.env.SOCKET_PORT || 3001;
 
 // Middleware para parsing de JSON
 app.use(express.json());
+
+app.use(bodyParser.json());
+app.use(cors({
+  origin: 'http://localhost:5173', // URL do frontend
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 
 // Usar as rotas
 app.use('/api', routes);
